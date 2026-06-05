29c8350b655312f3cd02f9d56660734feda13336
EC2 Default User <ec2-user@ip-172-31-34-17.ap-south-1.compute.internal>
2026-06-05 04:39:46 +0000

    Added modern animated website

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Website Loaded");
+document.querySelector("button").addEventListener("click", () => {
+    document.getElementById("about").scrollIntoView({
+        behavior: "smooth"
+    });
+});
+
+console.log("CloudNova Website Loaded");
